c3982ca514d6cd3845e6edaa0b316f81acd2a3ea
Nishit Vats <nishitvats@icloud.com>
2025-02-26 16:43:26 +0530

    Initial Commit

---
+++ LevelFit/LevelFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelFitApp.swift
+//  LevelFit
+//
+//  Created by Nishit Vats on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LevelFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
