f9aeb8b34f3e4dc3fe79fb800f03df31c554d628
Nishit Vats <nishitvats@icloud.com>
2025-03-02 12:45:32 +0530

    Update

--- LevelFit/ContentView.swift
+++ LevelFit/ContentView.swift
@@ -11,25 +11,13 @@ struct ContentView: View {
     @State var tabSelcted:SelectedTab = .home
     init() {
            let appearance = UITabBarAppearance()
-           
-           // Background color
            appearance.backgroundColor = UIColor(.B_3)
-           
-           // Active tab item color
            appearance.stackedLayoutAppearance.selected.iconColor = UIColor.white
            appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
-           
-           // Inactive tab item color
            appearance.stackedLayoutAppearance.normal.iconColor = UIColor.gray
            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.gray]
-           
-           
            appearance.shadowColor = UIColor.black.withAlphaComponent(0.2)
-           
-           // Customize unselected opacity (optional)
            UITabBar.appearance().unselectedItemTintColor = UIColor.gray
-           
-           // Apply appearance to all tab styles
            UITabBar.appearance().standardAppearance = appearance
            UITabBar.appearance().scrollEdgeAppearance = appearance
        }
